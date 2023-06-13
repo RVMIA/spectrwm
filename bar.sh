00c15025bff6c22e0093a8f33a7e557b997b96e9
ame <elliotdtx@gmail.com>
2023-06-12 19:03:39 -0500

    first commit, copies xmonad config

---
+++ bar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while :; do
+    pkg=$(~/.config/scripts/packages.sh)
+    spt=$(~/.config/scripts/spotify.sh)
+    mem=$(~/.config/scripts/mem.sh)
+    net=$(~/.config/scripts/network.sh)
+    date=$(~/.config/scripts/date.sh)
+    cpu=$(~/.config/scripts/cpu.sh)
+    wttr=$(~/.config/scripts/wttr.sh)
+    echo "[ $pkg | $spt | $cpu | $mem | $net | $wttr | $date ]"
+    sleep 0.3
+done
